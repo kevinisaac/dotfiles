c0520376473023c29325a064c8a5b00181a7e4e6
Kevin Isaac <kevin.isaac70@gmail.com>
2015-08-20 13:56:04 +0000

    Added the .vim directory

---
+++ .vim/bundle/vim-snippets/snippets/TEST.java
@@ -0,0 +1,9 @@
+Scanner scan = new Scanner(System.in);
+System.out.println(Hello World!);System.out.println(scan);;;;;;;;;;;;;;;;;;;;;
+public class TEST{
+    public static void main(String[] args){
+        System.out.println("Hello Kevin");
+        Scanner scan = new Scanner(System.in);
+        :q!
+    }
+}
