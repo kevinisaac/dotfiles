13697295c699cec51e8ca128daa3ab049d3d9506
Kevin Isaac <kevin.isaac70@gmail.com>
2017-09-12 18:39:55 +0530

    Added systemctl shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,7 @@
 set -x BROWSER chromium
 set -x EDITOR vim
-# set -x STAGING_WRITECOPY "1"
-# set -x TERMINAL gnome-terminal
 set -x TERMINAL urxvt
-set -x TERMINUS_DEV_CONF /home/kevin/Code/python/flask/xlate/config/development.py
 set -x ttymouse xterm2
-# bash ~/.local/bin/bashmarks.sh
-# set -x PYTHONSTARTUP /home/kevin/Code/python/scripts/pretty_json.py
 
 # Set the key repeat rate
 xset r rate 160 60
@@ -17,24 +12,8 @@ xset -b
 # Silence the blind laptop screen
 # xrandr --output LVDS-1 --off
 
-function fish_greeting
-#     fortune phrases vocabulary
-end
-
 # Load fishmarks (http://github.com/techwizrd/fishmarks)
 . $HOME/.fishmarks/marks.fish
-
-function startterminology
-    cd /home/kevin/Code/terminology
-    . venv/bin/activate.fish
-    python run.py
-end
-
-function startzucumber
-    cd /home/kevin/Code/zucumber
-    . venv/bin/activate.fish
-    python manage.py runserver
-end
 
 # start X at login
 if status --is-login
@@ -43,24 +22,47 @@ if status --is-login
     end
 end
 
-# . /usr/lib/python3.4/site-packages/powerline/bindings/fish/powerline-setup.fish
-# powerline-setup
+# Shortcut for Systemctl commands
+function sstart
+    sudo systemctl start $argv
+end
 
-# Start the powerline daemon to prevent lag
-# powerline-daemon -q
+function sstop
+    sudo systemctl stop $argv
+end
 
-# Path to Oh My Fish install.
-# set -gx OMF_PATH /home/kevin/.local/share/omf
+function sstatus
+    sudo systemctl status $argv
+end
 
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /home/kevin/.config/omf
+function srestart
+    sudo systemctl restart $argv
+end
 
-# Load oh-my-fish configuration.
-# source $OMF_PATH/init.fish
-
-###########################################
-
-# set -gx PATH "/usr/local/bin" "." "/home/kevin/.gem/ruby/2.3.0/bin" $PATH
-
-# alias vim nvim
 alias n ncmpcpp
+
+
+# Shortcuts to start apps
+function startterminology
+    cd /home/kevin/Code/terminology
+    . venv/bin/activate.fish
+    python run.py
+end
+
+function startlighttables
+    cd /home/kevin/Code/lighttables
+    . venv/bin/activate.fish
+    python run.py
+end
+
+function startlistino
+    cd /home/kevin/Code/listino
+    . venv/bin/activate.fish
+    python run.py
+end
+
+function startzucumber
+    cd /home/kevin/Code/portalapp1
+    . venv/bin/activate.fish
+    env ENV_TYPE="01" python manage.py runserver
+end
